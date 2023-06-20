8dde1920a7ce9dd4845fe715e048b5d9b468082c
JanneT <janne.tuhkanen@hotmail.com>
2023-06-20 07:19:30 +0300

    Changed logout to happen on get request on logout page.

--- Syntax/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Syntax/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -23,21 +23,13 @@ namespace Syntax.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
         }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        
+        public async Task<IActionResult> OnGet()
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+            
+            return RedirectToPage(Url.Page("/"));
         }
     }
 }
