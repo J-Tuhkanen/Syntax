7ee49851a25dbd8a2ff6915d135aa4641e8f6a08
Ezcoo <ezcoo99@gmail.com>
2023-09-02 21:20:05 +0300

    Add post deletion

--- Syntax.Core/Repositories/Base/IPostRepository.cs
+++ Syntax.Core/Repositories/Base/IPostRepository.cs
@@ -5,6 +5,7 @@ namespace Syntax.Core.Repositories.Base
     public interface IPostRepository
     {
         Task<Post> CreatePostAsync(Post post);
+        Task<Post> DeletePostAsync(string id);
         Task<Post> GetPostById(string id);
         Task<IEnumerable<Post>> GetPostsAsync(IEnumerable<string> excludedPosts, int amount);
         Task<IEnumerable<Post>> GetPostsByUserAsync(string userId, IEnumerable<string> excludedPosts, int amount);
