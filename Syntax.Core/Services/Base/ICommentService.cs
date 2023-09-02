48e05268d850ece4a1e8b6126fbc54a7fb2d56fb
Ezcoo <ezcoo99@gmail.com>
2023-09-02 21:23:38 +0300

    Update comment deletion

--- Syntax.Core/Services/Base/ICommentService.cs
+++ Syntax.Core/Services/Base/ICommentService.cs
@@ -7,7 +7,7 @@ namespace Syntax.Core.Services.Base
     public interface ICommentService
     {
         Task<Comment> CreateCommentAsync(string postId, string content, string userId);
-        Task<bool> DeleteCommentAsync(string id);
+        Task<Comment> DeleteCommentAsync(string id);
         Task<Comment> GetCommentAsync(string id);
         Task<IEnumerable<Comment>> GetCommentsAsync(string postId, IEnumerable<string> ExcludedComments, int amount = 5);
         Task<IEnumerable<Comment>> GetCommentsByUserAsync(string userId, IEnumerable<string> ExcludedComments, int amount = 5);
