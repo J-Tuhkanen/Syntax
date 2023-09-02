f09e707b6a735f3f62dcb09b523d318a335ac2c9
Ezcoo <ezcoo99@gmail.com>
2023-09-02 21:16:10 +0300

    Remove appDbContext & delegate post deletion to repository pattern

--- Syntax.Core/Services/PostService.cs
+++ Syntax.Core/Services/PostService.cs
@@ -13,12 +13,10 @@ namespace Syntax.Core.Services
     public class PostService : IPostService
     {
         private readonly IPostRepository _postRepository;
-        private ApplicationDbContext _appDbContext;
 
-        public PostService(ApplicationDbContext appDbContext, IPostRepository postRepository)
+        public PostService(IPostRepository postRepository)
         {
             _postRepository = postRepository;
-            _appDbContext = appDbContext;
         }
 
         public async Task<Post> CreatePostAsync(string title, string body, string userId)
@@ -48,19 +46,7 @@ namespace Syntax.Core.Services
         /// </summary>
         public async Task<Post> DeletePostAsync(string id)
         {
-            // Get post from database
-            var post = _appDbContext.Posts.FirstOrDefault(p => p.Id == id);
-
-            // If post was found, mark it as deleted.
-            if (post == null)
-            {
-                throw new Exception($"Post with id {id} was not found and could not be deleted.");
-            }
-
-            post.IsDeleted = true;
-            await _appDbContext.SaveChangesAsync();
-
-            return post;
+            return await _postRepository.DeletePostAsync(id);
         }
     }
 }
