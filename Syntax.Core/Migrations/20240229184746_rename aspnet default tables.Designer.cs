ebbf1d0eade97523041d6b18ab0c3ab46bb23fca
JanneT <janne.tuhkanen@hotmail.com>
2024-02-29 21:09:49 +0200

    Renamed default asp.net table names

--- Syntax.Core/Migrations/20240229184746_rename aspnet default tables.Designer.cs
+++ Syntax.Core/Migrations/20240229184746_rename aspnet default tables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Syntax.Core.Data;
@@ -11,9 +12,10 @@ using Syntax.Core.Data;
 namespace Syntax.Core.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240229184746_rename aspnet default tables")]
+    partial class renameaspnetdefaulttables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
